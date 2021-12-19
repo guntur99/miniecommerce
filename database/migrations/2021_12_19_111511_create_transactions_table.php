a977786cd99e49a3105f8daa13456700b4a268d2
guntur99 <gugunguntur99@gmail.com>
2021-12-19 20:47:25 +0700

    add transactions table & status seeder

---
+++ database/migrations/2021_12_19_111511_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->string('transaction_code');
+            $table->string('amount');
+            $table->foreignId('status')->constrained('transaction_status')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
---
+++ database/migrations/2021_12_19_113544_create_transaction_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
